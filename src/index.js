74cce70bc3172a24775f709758445ef050f8e619
dawncronin <github email address>
2019-12-11 10:06:01 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.querySelector('form');
+
+
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(event.target)
+    const inputField = document.querySelector('#new-task-description')
+    const task = inputField.value
+    const toDo = document.querySelector("#tasks")
+    const newTask = document.createElement("Li")
+    newTask.innerText = task
+    toDo.appendChild(newTask)
+    inputField.value = ""
+
+    ;
+  });
+
+
 });
+
+
